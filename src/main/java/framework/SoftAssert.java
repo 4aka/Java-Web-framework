b94bb31e146e91ec068c7c64845136ab0686c992
4aka <7nuclear@gmail.com>
2024-12-18 17:50:50 +0200

    Add soft isDisplayed()

--- src/main/java/framework/SoftAssert.java
+++ src/main/java/framework/SoftAssert.java
@@ -1,8 +1,21 @@
 package framework;
 
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
 import org.testng.asserts.Assertion;
 
 public class SoftAssert extends Assertion {
+
+    public static boolean isElementDisplayedSoft(WebElement element) {
+        boolean result = true;
+
+        try {
+            element.isDisplayed();
+        } catch (NoSuchElementException ignored) {
+            result = false;
+        }
+        return result;
+    }
 
 }
 
